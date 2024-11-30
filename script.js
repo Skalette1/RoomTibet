c201692b7e7aa485e843ade4f0b67f373738e9eb
Beslan <beslandudaev8@gmail.com>
2024-11-30 22:55:36 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// const img = document.querySelector('.img-container-foot');
+// const lupa = document.getElementById('zoom-icon');
+
+// img.addEventListener('mouseover', () => {
+//     lupa.style.display = 'block'
+// });
+
+// img.addEventListener('mouseout',() => {
+//     lupa.style.display ='none'
+// });
+
