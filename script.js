9b4a42bf5b4c352c01002dbb4e3630c4fa5626ef
Beslan <beslandudaev8@gmail.com>
2024-12-25 11:54:59 +0300

    last commit

--- script.js
+++ script.js
@@ -9,3 +9,25 @@
 //     lupa.style.display ='none'
 // });
 
+let num1 = (23 + 97);
+let num2 = (4 + 6 + 9)/77 ;
+console.log(num1 + num2);
+
+
+// const message = "Вон отсюда";
+// alert(message);
+
+// const a = 10;
+// console.log(a);
+// console.log(9*a);
+// console.log(++a);
+
+
+// const MAX = 57;
+// const actual = MAX - 13;
+// const per = actual / MAX;
+
+
+let w = 4;
+console.log(w++);
+console.log(++w)
